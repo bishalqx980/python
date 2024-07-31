2a6623e2dcb8ee7ac2383045e24c50c011853f2a
bishalqx980 <bishalqx680@gmail.com>
2024-07-31 22:03:41 +0600

    update

---
+++ misc_modules/README.md
@@ -0,0 +1,3 @@
+# modules
+
+- [modules](https://github.com/bishalqx980/tgbot/tree/main/bot/modules)
--- misc_modules/utils.py
+++
@@ -1,9 +0,0 @@
-from bot import logger
-
-async def calculator(math):
-    try:
-        solved_math = eval(math)
-        return True, solved_math
-    except Exception as e:
-        logger.error(e)
-        return False, e
